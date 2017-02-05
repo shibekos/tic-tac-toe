facb004c4c857034dd0456a7d7a5f509dde7326a
N. Shibeko <nikit.shibeko@gmail.com>
2017-02-05 20:32:45 +0300

    new

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,14 +1,32 @@
 class TicTacToe {
     constructor() {
-
+        this.namePlayer = 1
+        this.field = [
+        [null,null,null],
+        [null,null,null],
+        [null,null,null]
+        ]
     }
 
     getCurrentPlayerSymbol() {
-
+        if (this.namePlayer === 1) {
+            return "x" {
+        } else {
+            return "o"
+        }
+      }
     }
 
     nextTurn(rowIndex, columnIndex) {
-
+        if (!this.Board[rowIndex][columnIndex]){
+            if(this.CurrentSymbol === 'x'){
+                this.Board[rowIndex][columnIndex] = 'x';
+                this.CurrentSymbol = 'o'; 
+            } else {
+                this.Board[rowIndex][columnIndex] = 'o';
+                this.CurrentSymbol = 'x';
+            }
+        }
     }
 
     isFinished() {
